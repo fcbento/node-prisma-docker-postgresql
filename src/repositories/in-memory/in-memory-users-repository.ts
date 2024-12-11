ea26d6ec01e63a1d4b95522e2aff8553bb8301cb
fcbento <felipe.16costa@gmail.com>
2024-12-10 22:55:05 -0300

    feat: add vitest and users repository, docker compose yml

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,36 @@
+import { User, Prisma, Role } from "@prisma/client";
+import { UsersRepository } from "../users-repository";
+import { randomUUID } from "node:crypto";
+
+export class InMemoryUsersRepository implements UsersRepository {
+
+  public users: User[] = []
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.users.find(user => user.email === email)
+    if(!user) return null
+    return user
+  }
+
+  async findById(id: string): Promise<User | null> {
+    const user = this.users.find(user => user.id === id)
+    if(!user) return null
+    return user
+  }
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: randomUUID(),
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      role: data.role as Role,
+      created_at: new Date()
+    }
+
+    this.users.push(user)
+    
+    return user
+  }
+
+}
---
+++ src/services/users/tests/register-user-service.spec.ts
@@ -0,0 +1,68 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { RegisterUserService } from './register-user-service'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import bcrypt from 'bcryptjs';
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
+
+let usersRepository: InMemoryUsersRepository
+let sut: RegisterUserService
+
+describe('Register user service', () => {
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterUserService(usersRepository)
+  })
+
+  it('should create user', async () => {
+    const { user } = await sut.execute({
+      name: 'Test',
+      email: 'testing@test.com',
+      password: '123456'
+    })
+    expect(user.id).toEqual(expect.any(String))
+  })
+
+  it('should hash user password uopn registration', async () => {
+
+    const { user } = await sut.execute({
+      name: 'John',
+      email: 'johdnd@email.com',
+      password: '123456'
+    })
+
+    const isPasswordCorrectlyHashed = await bcrypt.compare('123456', user.password_hash)
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not be able to register user', async () => {
+
+    const { user } = await sut.execute({
+      name: 'Test',
+      email: 'testing@email.com',
+      password: '654321'
+    })
+
+    const isPasswordCorrectlyHashed = await bcrypt.compare('123456', user.password_hash)
+    expect(isPasswordCorrectlyHashed).toBe(false)
+  })
+
+  it('should not be able to register with same email', async () => {
+
+    const email = 'test@email.com'
+
+    await sut.execute({
+      name: 'Test',
+      email,
+      password: '123456'
+    })
+    
+    await expect(() => 
+      sut.execute({
+        name: 'John',
+        email,
+        password: '123456'
+      }),
+    )?.rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+})
